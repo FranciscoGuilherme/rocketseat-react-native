1eeb4283ee345ad78379bb63b15e3536abd4e3c2
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2025-12-21 19:36:06 -0300

    feat(home): :sparkles: adiciona o componente Filter na home

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,6 +1,8 @@
 import { View, Image } from "react-native"
 import { Input } from "@/components/Input"
 import { Button } from "@/components/Button"
+import { Filter } from "@/components/Filter"
+import { FilterStatus } from "@/types/FilterStatus"
 import { styles } from "./styles"
 
 export function Home() {
@@ -14,7 +16,8 @@ export function Home() {
       </View>
 
       <View style={styles.content}>
-
+        <Filter status={FilterStatus.DONE} isActive={true} />
+        <Filter status={FilterStatus.PENDING} isActive={false} />
       </View>
     </View>
   )
