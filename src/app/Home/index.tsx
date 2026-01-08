2f65c64ae94503599135d1fd8957538bfaf56d0f
Francisco Guilherme <franciscoguilherme.mundo@outlook.com>
2026-01-07 22:17:17 -0300

    feat(home): :sparkles: adiciona o componente Item a Home

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,5 @@
 import { View, Image, TouchableOpacity, Text } from "react-native"
+import { Item } from "@/components/Item"
 import { Input } from "@/components/Input"
 import { Button } from "@/components/Button"
 import { Filter } from "@/components/Filter"
@@ -27,6 +28,12 @@ export function Home() {
             <Text style={styles.clearText}>Limpar</Text>
           </TouchableOpacity>
         </View>
+
+        <Item
+          data={{ status: FilterStatus.PENDING, description: "Comprar leite" }}
+          onStatus={() => console.log("mudar o status")}
+          onRemove={() => console.log("remover")}
+        />
       </View>
     </View>
   )
